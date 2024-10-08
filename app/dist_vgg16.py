95132a589085919ccca4499608b130afb1f418a1
asu <asueeer@163.com>
2024-10-08 09:17:18 +0000

    pretrain is false

--- app/dist_vgg16.py
+++ app/dist_vgg16.py
@@ -69,7 +69,7 @@ if os.path.exists(model_path):
     model.load_state_dict(torch.load(model_path)) 
 else:
     print("Downloading and saving VGG16 pretrained model weights...")
-    model = models.vgg16(pretrained=True)  # 下载预训练模型
+    model = models.vgg16(pretrained=False)  # 下载预训练模型
     torch.save(model.state_dict(), model_path)  # 保存预训练权重到本地
     
 def evaluate(model, test_loader, criterion):
